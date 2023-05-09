9d11bb19f06989a3328d8eef98e16237a704e434
javier <jbotellalecanda@gmail.com>
2023-05-09 10:15:29 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import Avengers from "./components/User/Avengers";
-import Terminator from "./components/Characters/Terminator";
-import SplitPane from "./components/Wrapper/SplitPane";
 
 const App = () => {
-  return (
-    <>
-      {/* <Avengers />
-      <Terminator /> */}
-      <SplitPane left={<Terminator />} right={<Avengers />} />
-    </>
-  );
+  
 };
 
 export default App;

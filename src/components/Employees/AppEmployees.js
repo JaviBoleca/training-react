97ca1d7b2653cc73efcb83ba478f9f3f4be08ab8
javier <jbotellalecanda@gmail.com>
2023-05-09 13:42:13 +0200

    employees

---
+++ src/components/Employees/AppEmployees.js
@@ -0,0 +1,91 @@
+import "./Employees";
+const AppEmployees = () => {
+  const Info = [
+    {
+      url_imagen: "https://ejemplo.com/juan_perez.jpg",
+      nombre: "Juan Pérez",
+      rol: "Cocinero",
+      salario_euros_hora: "7 euros/hora",
+      num_reviews: 15,
+    },
+    {
+      url_imagen: "https://ejemplo.com/maria_garcia.jpg",
+      nombre: "María García",
+      rol: "Camarera",
+      salario_euros_hora: "8 euros/hora",
+      num_reviews: 10,
+    },
+    {
+      url_imagen: "https://ejemplo.com/pedro_romero.jpg",
+      nombre: "Pedro Romero",
+      rol: "Recepcionista",
+      salario_euros_hora: "9 euros/hora",
+      num_reviews: 5,
+    },
+    {
+      url_imagen: "https://ejemplo.com/carla_gomez.jpg",
+      nombre: "Carla Gómez",
+      rol: "Limpiadora",
+      salario_euros_hora: "6 euros/hora",
+      num_reviews: 20,
+    },
+    {
+      url_imagen: "https://ejemplo.com/antonio_sanchez.jpg",
+      nombre: "Antonio Sánchez",
+      rol: "Jardinero",
+      salario_euros_hora: "7 euros/hora",
+      num_reviews: 12,
+    },
+    {
+      url_imagen: "https://ejemplo.com/laura_martinez.jpg",
+      nombre: "Laura Martínez",
+      rol: "Auxiliar de cocina",
+      salario_euros_hora: "7.5 euros/hora",
+      num_reviews: 8,
+    },
+    {
+      url_imagen: "https://ejemplo.com/david_garcia.jpg",
+      nombre: "David García",
+      rol: "Barman",
+      salario_euros_hora: "10 euros/hora",
+      num_reviews: 15,
+    },
+    {
+      url_imagen: "https://ejemplo.com/sara_perez.jpg",
+      nombre: "Sara Pérez",
+      rol: "Encargada",
+      salario_euros_hora: "12 euros/hora",
+      num_reviews: 7,
+    },
+    {
+      url_imagen: "https://ejemplo.com/fernando_martin.jpg",
+      nombre: "Fernando Martín",
+      rol: "Maitre",
+      salario_euros_hora: "15 euros/hora",
+      num_reviews: 10,
+    },
+    {
+      url_imagen: "https://ejemplo.com/marta_gomez.jpg",
+      nombre: "Marta Gómez",
+      rol: "Ayudante de camarera",
+      salario_euros_hora: "7 euros/hora",
+      num_reviews: 18,
+    },
+    {
+      url_imagen: "https://ejemplo.com/pablo_sanchez.jpg",
+      nombre: "Pablo Sánchez",
+      rol: "Encargado de limpieza",
+      salario_euros_hora: "9 euros/hora",
+      num_reviews: 6,
+    },
+    {
+      url_imagen: "https://ejemplo.com/maria_garcia.jpg",
+      nombre: "María García",
+      rol: "Camarera",
+      salario_euros_hora: "8 euros/hora",
+      num_reviews: 10,
+    },
+  ];
+  return( <Employees 
+    
+};

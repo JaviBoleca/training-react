8726ec9b00976c5d7ff20b5d6d59a4b8c5d76e63
javib <jbotellalecanda@gmail.com>
2023-05-09 19:49:41 +0200

    cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./Card.js"
+import Card from "./components/Card";
+import freelance from "./db/freelance.json";
 
 const App = () => {
-  return }
+  return (
+    <>
+      {freelance.map((card, index) => {
+        return (
+          <Card
+            key={index}
+            image={card.image}
+            title={card.title}
+            lastname={card.lastname}
+            role={card.role}
+            fee={card.fee}
+            reviews={card.reviews}
+          />
+        );
+      })}
+    </>
+  );
+};
 
-  
 export default App;
---
+++ src/components/cardsV1/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import CardWrapper from "./components/Card/CardWrapper";
+import CardImg from "./components/Card/CardImg";
+import CardInfo from './components/Card/CardInfo';
+import CardsContainer from './components/Card/CardsContainer';
+import { freelancers } from './db/freelancers';
+
+function App() {
+
+  return (
+
+    <CardsContainer>
+      {freelancers.map((freelancer, index) => {
+        return (
+          <CardWrapper key={index}>
+            <CardImg freelancer={freelancers[index]}></CardImg>
+            <CardInfo freelancer={freelancers[index]}></CardInfo>
+          </CardWrapper>
+        )
+      })}
+
+    </CardsContainer>
+
+  );
+}
+
+export default App;

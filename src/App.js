0cfae8e843a28530434478665a8a0cfaef09aa46
javib <jbotellalecanda@gmail.com>
2023-05-16 18:53:51 +0200

    styles-modules

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ const Title = styled.h1`
   transition: all 0.25s ease-in;
 `;
 
-const Wrapper = styled.section`
-  font-size: 1.5em;
+const Wrapper = styled.section.attrs((props) => ({
+  id: "header",
+  size: props.size || "1.3em",
+  className: props.darkTheme && "dark",
+  title: props.title || "header section",
+}))`
+  &:hover h1 {
+    color: ${(props) => (props.darkTheme ? "PapayaWhip" : "PeachPuff")};
+  }
+  h2 {
+    background-color: ${(props) =>
+      props.darkTheme ? "PapayaWhip" : "PeachPuff"};
+    border-radius: 0.5rem;
+    margin-bottom: 1.5em;
+  }
+  font-size: ${(props) => props.size};
   padding: 4em;
   background-color: ${(props) =>
     props.darkTheme ? "DarkSlateGray" : "DarkOliveGreen"};
@@ -39,8 +53,9 @@ const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <Wrapper darkTheme={darkTheme}>
+    <Wrapper darkTheme={darkTheme} size="1.5em" title="Main Section">
       <Title darkTheme={darkTheme}>Hello World!</Title>
+      <h2>Styled Components</h2>
       <Paragraph darkTheme={darkTheme}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </Paragraph>

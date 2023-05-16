8eeb1d9b2445fe9d0d7bddb60597de14fb20a25b
javib <jbotellalecanda@gmail.com>
2023-05-16 17:25:06 +0200

    darkmodeOk

--- src/App.js
+++ src/App.js
@@ -1,93 +1,54 @@
 import styled from "styled-components";
-
 import { useState } from "react";
 
-
-
 const Title = styled.h1`
-
-  font-size: 1.5em;
-
+  font-size: 3.5em;
   text-align: center;
-
-  color: AliceBlue;
-
+  color: ${(props) => (props.darkTheme ? "AliceBlue" : "#282828")};
   margin-bottom: 1em;
-
+  transition: all 0.25s ease-in;
 `;
 
-
-
 const Wrapper = styled.section`
-
   font-size: 1.5em;
-
   padding: 4em;
-
   background-color: ${(props) =>
-
     props.darkTheme ? "DarkSlateGray" : "DarkOliveGreen"};
-
   text-align: center;
-
+  transition: all 0.25s ease-in;
 `;
 
 const Paragraph = styled.p`
-
-  background-color: DarkCyan;
-
-  color: White;
-
-  border-radius: 0.5rem;
-
+  background-color: ${(props) => (props.darkTheme ? "Snow" : "DarkCyan")};
+  color: ${(props) => (props.darkTheme ? "#282828" : "Snow")};
   padding: 1em;
-
+  border-radius: 0.5rem;
   margin-bottom: 1em;
-
+  transition: all 0.25s ease-in;
 `;
 
 const Button = styled.button`
-
-  border-radius: 0.3rem;
-
-  background-color: LightBlue;
-
-  padding: 1em 1.5em;
-
   font-size: 1.3rem;
-
-  `;
+  border-radius: 0.3rem;
+  background-color: ${(props) => (props.darkTheme ? "LightBlue" : "SteelBlue")};
+  padding: 1em 1.5em;
+  transition: all 0.25s ease-in;
+`;
 
 const App = () => {
-
-  
-
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-
     <Wrapper darkTheme={darkTheme}>
-
-      {" "}
-
-      {/* darkTheme es prop, sino no se pone el props, cambiará de color */}
-
-      <Title>Hello World!</Title>
-
-      <Paragraph>
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, iure.
-
+      <Title darkTheme={darkTheme}>Hello World!</Title>
+      <Paragraph darkTheme={darkTheme}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </Paragraph>
-
-      <Button onClick={() => setDarkTheme(!darkTheme)}>Change Theme</Button>
-
+      <Button darkTheme={darkTheme} onClick={() => setDarkTheme(!darkTheme)}>
+        {darkTheme ? "Light Theme" : "Dark Theme"}
+      </Button>
     </Wrapper>
-
   );
-
 };
-
-
 
 export default App;

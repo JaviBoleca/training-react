f61e329fc040eb30f21acf027f8bda5de3cc7e9f
javier <jbotellalecanda@gmail.com>
2023-05-17 17:06:14 +0200

    codigo-antonio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import Input from "./componentes/Task/TaskInput";
-import List from "./componentes/Task/TaskList";
-// import DB from "./mockedDB/task.json"
-
-import { useState } from "react";
+import { useState } from 'react';
+import data from "./mockedDB/tasks.json"
+import TaskList from './components/Tasks/TasksList';
+import TasksInput from './components/Tasks/TasksInput';
+import classes from './App.module.css';
 
 const App = () => {
-    const [tasks, setTasks] = useState("");
-    console.log(tasks);
+  const [tasks, setNewTask] = useState(data);
 
-    const addTask = (task) => {
-        setTasks([...tasks, task])};
+  const addTaskHandler = enteredText => {
+    setNewTask(prevTasks => {
+      const updatedTasks = [...prevTasks];
+      updatedTasks.unshift({ text: enteredText, id: Math.random().toString(16).substring(2) });
+      return updatedTasks;
+    });
+  };
+
+  const deleteItemHandler = taskId => {
+    setNewTask(prevTasks => {
+      const updatedTasks = prevTasks.filter(task => task.id !== taskId);
+      return updatedTasks;
+    });
+  };
+
+  let content = (
+    <h2 style={{ textAlign: 'center', padding: '1em', border: '1px solid #339900', backgroundColor: '#99cc33' }}>No tasks availables. Add one?</h2>
+  );
+
+  if (tasks.length > 0) {
+    content = (
+      <TaskList items={tasks} onDeleteItem={deleteItemHandler} />
+    );
+  }
 
   return (
-    <>
-        <Input task ={addTask}/>
-    </>
+    <main>
+      <section className={classes["task-form"]}>
+        <TasksInput onAddTask={addTaskHandler} />
+      </section>
+      <section className={classes["tasks-content"]}>
+        {content}
+      </section>
+    </main>
   );
 };
+
 export default App;
--- src/componentes/UI/Button/Button.js
+++
@@ -1,5 +0,0 @@
-const Button = (props) => {
-    return(
-        <button onClick={()=>{task()}}></button>
-    )
-};
